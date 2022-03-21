9a4234331cea6c510711e819a090977d975d3dfa
luvero <luciana_lr@hotmail.com>
2022-03-21 02:18:04 -0300

    localStorageYJson

--- lyp.js
+++ lyp.js
@@ -5,8 +5,9 @@ class Calzado {
     this.precio = calzado.precio;
     this.cantidad = 1;
     this.numero = calzado.numero;
-    this.tipo= calzado.tipo
+    this.tipo= calzado.tipo;
     this.precioTotal= calzado.precio;
+    this.img = calzado.img;
     }
 
     agregarProducto(){
@@ -115,8 +116,8 @@ function imprimirProductosHTML(calzados){
     let containerComprarItems = document.getElementById("container-comprar--items");
     for( const calzado of calzados){
         let card = document.createElement("div");
-        card.innerHTML=` <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-12">
+        card.innerHTML=`
+               
                     <div class="card bg-warning text-light bg-gradient" style="width: 18rem;">
                         <img src="${calzado.img}" id="" class="card-img-top" width="170px" height="250"
                             alt="calzados1">
@@ -124,9 +125,8 @@ function imprimirProductosHTML(calzados){
                             <h5 class="card-title"> ${calzado.tipo} </h5>
                             <p class="card-text">${calzado.precio}</p>
                             <button id="Comprar${calzado.id}" type="button" onclick=""
-                            class=" btn btn-danger">COMPRAR</button>
+                            class="comprar">COMPRAR</button>
                         </div>
-                    </div>
                 </div>`
     
 containerComprarItems.appendChild(card);
@@ -135,6 +135,27 @@ let boton = document.getElementById( `Comprar${calzado.id}`);
 boton.onclick= () => agregarAlcarrito(calzado.id);
 
 }
+}
+
+//STORAGE
+
+function chequearCarritoStorage (){
+    let contenidoEnStorage= JSON.parse(localStorage.getItem("carritoEnStorage"));
+    console.log("contenido en chequear Carrito en ls", contenidoEnStorage);
+
+    if(contenidoEnStorage){
+        let array=[];
+        for(let i = 0 ; i < contenidoEnStorage.length; i++ ) {
+            let calzado = new Calzado(
+                contenidoEnStorage[i],
+                contenidoEnStorage[i].cantidad);
+                calzado.ultimoPrecioTotal();
+                array.push(calzado);
+
+        }
+return array;
+    }
+    return[];
 }
 
 function agregarAlcarrito (idProducto) {
@@ -149,6 +170,7 @@ function agregarAlcarrito (idProducto) {
     else{
         carrito.push(new Calzado(calzados[idProducto]));
     }
+    localStorage.setItem("carritoEnStorage", JSON.stringify(carrito));
     descripcionTabla(carrito);
 }
 
@@ -174,6 +196,7 @@ function eliminarDeCarrito(id){
             carrito= [];
         }
     }
+    localStorage.setItem("carritoEnStorage", JSON.stringify(carrito));
     descripcionTabla(carrito);
 }
 
@@ -193,25 +216,25 @@ function descripcionTabla (array){
     listaTable.innerHTML = `<table id="tablaCarrito" class="table">
             <thead>
                 <tr>
-                
-                    <th scope="col">Descripcion</th>
-                    <th scope="col">Cantidad</th>
-                    <th scope="col">Color</th>
-                    <th scope="col">Precio </th>
-                    <th scope="col">Accion</th>
+                    <th>Imagen</th>
+                    <th>Descripcion</th>
+                    <th>Cantidad</th>
+                    <th>Color</th>
+                    <th>Precio </th>
+                    <th>Accion</th>
                 </tr>
             </thead>
             <tbody id="bodyTabla">
-                <tr>
-                    <td>Total: $${precioTotal}</td>
-                    <td> </td>
-                    <td> </td>
-                    <td> </td>
-                </tr>
-            <tr> 
-                <td> <button id="vaciarCarrito" class="btn btn-danger "> Vaciar Carrito </button> </td>
-            </tr>
             </tbody>
+                
+            <tr> 
+                <td> <button id="vaciarCarrito" class="btn btn-danger"> Vaciar Carrito </button> </td>
+            </tr>
+            <tr>
+            <td class="text-end fs-3 fw-bold">Total: $${precioTotal}</td>
+            </tr>
+
+            
         </table>
     `; 
 
@@ -225,14 +248,15 @@ let bodyTabla = document.getElementById("bodyTabla");
 for (let calzado of array){
     let item = document.createElement("div");
     item.innerHTML=`
-                <tr>
-                <th scope="row"></th>
-                    <td>${calzado.tipo}</td>
-                    <td>${calzado.cantidad}</td>
-                    <td>${calzado.color}</td>
-                    <td>${calzado.precio}</td>
-                    <td> <button id="eliminar${calzado.id}" type="button" class="btn btn-darger"> Eliminar </button> </td>
-                </tr>`;
+                <td class="imag">
+                <img src="${calzado.img}" width="100">
+                </td>
+
+                    <td class"tipo">${calzado.tipo}</td>
+                    <td class"cant">${calzado.cantidad}</td>
+                    <t class="color">${calzado.color}</td>
+                    <td class="precio">${calzado.precio}</td>
+                    <td class="elim"> <button id="eliminar${calzado.id}" type="button" class="btn btn-darger"> Eliminar </button> </td>`;
 
                 bodyTabla.appendChild(item);
 
@@ -263,6 +287,8 @@ function obtenerPrecioTotal(array) {
 }
 
 imprimirProductosHTML(calzados);
+carrito= chequearCarritoStorage();
+
 
 
 
